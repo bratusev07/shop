b94153cf6a2aff46a838ddf57d1146f64216d5cf
Денис Братусев <55459712+bratusev07@users.noreply.github.com>
2025-03-21 20:04:27 +0300

    Ui fix

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react'
-import Item from "./Item"
+import React from 'react'
 
-export class Items extends Component {
+export class Items extends React.Component {
   render() {
     return (
       <main>
         {this.props.items.map(el => (
-            <Item onShowItem={this.props.onShowItem} key={el.id} item={el} onAdd={this.props.onAdd}/>
+          <div 
+            className='item' 
+            key={el.id} 
+            onClick={() => this.props.onShowItem(el)}
+          >
+            <img src={el.img} />
+            <h2>{el.title}</h2>
+            <p>{el.description}</p>
+            <b>{el.price}</b>
+            <div 
+              className='add-to-cart' 
+              onClick={(e) => {
+                e.stopPropagation();
+                this.props.onAdd(el);
+              }}
+            >
+              +
+            </div>
+          </div>
         ))}
       </main>
     )
